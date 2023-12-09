a559cc10c42c05390b1fc038cec5f208ac7b6e9f
Denis Zolotarev <judaeus@gmail.com>
2023-12-09 15:24:44 +0200

    * change FactoryConfiguration

--- src/main/java/com/game/utils/FactoryConfiguration.java
+++ src/main/java/com/game/utils/FactoryConfiguration.java
@@ -4,14 +4,29 @@ import com.game.entity.Player;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
+import java.io.IOException;
+import java.util.Properties;
+
 public class FactoryConfiguration {
     private static FactoryConfiguration factoryConfiguration;
     private final SessionFactory sessionFactory;
 
     private FactoryConfiguration() {
+
+        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+
+        Properties properties = new Properties();
+        try {
+            properties.load(classLoader.getResourceAsStream("hibernate.properties"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
         sessionFactory = new Configuration()
+                .addProperties(properties)
                 .addAnnotatedClass(Player.class)
                 .buildSessionFactory();
+
     }
 
     public static FactoryConfiguration getInstance() {
